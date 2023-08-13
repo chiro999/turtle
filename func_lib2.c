eb36244fb5342ec79371345dac3600feafed993c
chiro999 <r.o.g.u.e@outlook.com>
2023-08-13 06:46:42 -0700

    lib2

--- func_lib2.c
+++ func_lib2.c
@@ -19,3 +19,23 @@ ssize_t str_write(char *str)
 	}
 	return (out_count);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}
