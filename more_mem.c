bc81327fbf04924d1bf808d6fd1d4f9a5c143b78
chiro999 <r.o.g.u.e@outlook.com>
2023-08-13 10:39:59 -0700

    100

---
+++ more_mem.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * mem_alloc - reallocates a pointer to increase the space
+ * @ptr: pointer to the old array
+ * @size: pointer to number of elements in the old array
+ *
+ * Return: pointer to the new array
+ */
+char **(char **ptr, size_t *size)
+{
+	char **new;
+	size_t i = 0;
+
+	new = malloc(sizeof(char *) * ((*size) + 20));
+	if (new == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	while (i < (*size))
+	{
+		new[i] = ptr[i];
+		i++;
+	}
+	*size += 20;
+	free(ptr);
+	return (new);
+}
