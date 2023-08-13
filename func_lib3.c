8477a8efb6fe88f75d3f5f9a4f5a135054f9d1c2
chiro999 <r.o.g.u.e@outlook.com>
2023-08-13 07:15:52 -0700

    3

--- func_lib3.c
+++ func_lib3.c
@@ -26,3 +26,36 @@ char *_strdup(char *replica)
 	}
 	return (clone);
 }
+
+/**
+ * int_to_string - converts an unsigned int to a string format
+ * @count: unsigned int to convert
+ *
+ * Return: pointer to the converted string
+ */
+char *int_to_string(unsigned int count)
+{
+	char *str;
+	unsigned int temp, digit;
+
+	temp = count;
+	/*storing numbers in reverse in memory*/
+	for (digit = 0; temp != 0; digit++)
+		temp /= 10;
+	str = malloc(sizeof(char) * (digit + 1));
+	if (str == NULL)
+	{
+		perror("Abandon Ship!!");
+		exit(127);
+	}
+	str[digit] = '\0';
+
+	/* reversing the array to get the true figure */
+	for (--digit; count; --digit)
+	{
+		str[digit] = (count % 10) + '0';
+		count /= 10;
+	}
+	return (str);
+}
+
