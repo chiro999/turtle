83e66d752852506f065e3a824a93c444caeccdac
chiro999 <r.o.g.u.e@outlook.com>
2023-08-14 10:38:59 -0700

    strtok

--- custom_strtok.c
+++ custom_strtok.c
@@ -2,13 +2,13 @@ User
 #include "shell.h"
 
 /**
- * matching - checks if a character matches any in a string
+ * is_a_match - checks if a character matches any in a string
  * @c: character to check
  * @str: string to check
  *
  * Return: 1 if match, 0 if not
  */
-unsigned int matching(char c, const char *str)
+unsigned int is_a_match(char c, const char *str)
 {
 	unsigned int i;
 
@@ -23,59 +23,59 @@ unsigned int matching(char c, const char *str)
 char *_strtok(char *str, const char *delim)
 {
     static char *token;
-    static char *next;
+    static char *next_token;
     unsigned int i;
 
-    /* Initialize 'next' with 'str' if provided */
+    /* Initialize 'next_token' with 'str' if provided */
     if (str != NULL)
-        next = str;
+        next_token = str;
 
-    /* Store the current value of 'next' in 'token' */
-    token = next;
+    /* Store the current value of 'next_token' in 'token' */
+    token = next_token;
 
     /* If 'token' is NULL, there are no more tokens, so return NULL */
     if (token == NULL)
         return (NULL);
 
     /* Find the start of the next token by skipping over delimiter characters */
-    for (i = 0; next[i] != '\0'; i++)
+    for (i = 0; next_token[i] != '\0'; i++)
     {
-        if (matching(next[i], delim) == 0)
+        if (is_a_match(next_token[i], delim) == 0)
             break;
     }
 
     /* If the end of the string or a '#' character is found, there are no more tokens */
-    if (next[i] == '\0' || next[i] == '#')
+    if (next_token[i] == '\0' || next_token[i] == '#')
     {
-        next = NULL;
+        next_token = NULL;
         return (NULL);
     }
 
     /* Update 'token' to point to the beginning of the next token */
-    token = next + i;
-    next = token;
+    token = next_token + i;
+    next_token = token;
 
     /* Find the end of the token by searching for the next delimiter */
-    for (i = 0; next[i] != '\0'; i++)
+    for (i = 0; next_token[i] != '\0'; i++)
     {
-        if (matching(next[i], delim) == 1)
+        if (is_a_match(next_token[i], delim) == 1)
             break;
     }
 
     /* If the end of the string is reached, set 'next' to NULL */
-    if (next[i] == '\0')
-        next = NULL;
+    if (next_token[i] == '\0')
+        next_token = NULL;
     else
     {
         /* Replace the delimiter character with a null terminator */
-        next[i] = '\0';
+        next_token[i] = '\0';
 
         /* Update 'next' to point to the character after the delimiter */
-        next += i + 1;
+        next_token += i + 1;
 
         /* If the next character is the end of the string, set 'next' to NULL */
-        if (*next == '\0')
-            next = NULL;
+        if (*next_token == '\0')
+            next_token = NULL;
     }
 
     /* Return a pointer to the current token */
