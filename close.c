b228cae9f0e5175515861cfdbd6bdc77a70deba2
chiro999 <r.o.g.u.e@outlook.com>
2023-08-15 07:24:14 -0700

    close

--- close.c
+++ close.c
@@ -8,22 +8,22 @@
 void close(input_t *input_variables)
 {
     int close_status; /* Declare a variable to store the exit status */
-    int index = 0;
+    int i = 0;
 
     /* Look for the "exit" command and check if an exit status is provided */
-    while (input_variables->tokens[index] != NULL)
+    while (input_variables->tokens[i] != NULL)
     {
-        if (_strncmp(input_variables->tokens[index], "exit") == 0 && input_variables->tokens[index + 1] != NULL)
+        if (_strncmp(input_variables->tokens[i], "exit") == 0 && input_variables->tokens[i + 1] != NULL)
         {
-            index++; // Move to the next token
-            close_status = _atoi(input_variables->tokens[index]); /* Convert the exit status string to an integer */
+            i++; // Move to the next token
+            close_status = _atoi(input_variables->tokens[i]); /* Convert the exit status string to an integer */
 
             /* Check if the conversion failed (non-numeric input) */
             if (close_status == -1)
             {
                 input_variables->status = 2; /* Set exit status to 2 (indicating an error) */
                 _error(input_variables, ": Unacceptable input: "); /* Call the error handling function */
-                str_out(input_variables->tokens[index]); /* Print the problematic input */
+                str_out(input_variables->tokens[i]); /* Print the problematic input */
                 str_out("\n"); /* Print a newline character */
                 free(input_variables->commands); /* Free allocated memory */
                 input_variables->commands = NULL; /* Set pointer to NULL */
@@ -32,7 +32,7 @@ void close(input_t *input_variables)
 
             input_variables->status = close_status; /* Set the exit status to the provided status */
         }
-        index++; // Move to the next token
+        i++; // Move to the next token
     }
 
     /* Free allocated memory */
