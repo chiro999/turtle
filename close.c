82af25ec61b0ac83dc490f249a13c4a5228c8b6b
chiro999 <r.o.g.u.e@outlook.com>
2023-08-15 07:22:11 -0700

    close

---
+++ close.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * close - Exit the program
+ * @input_variables: Pointer to input variables
+ * Return: void
+ */
+void close(input_t *input_variables)
+{
+    int close_status; /* Declare a variable to store the exit status */
+    int index = 0;
+
+    /* Look for the "exit" command and check if an exit status is provided */
+    while (input_variables->tokens[index] != NULL)
+    {
+        if (_strncmp(input_variables->tokens[index], "exit") == 0 && input_variables->tokens[index + 1] != NULL)
+        {
+            index++; // Move to the next token
+            close_status = _atoi(input_variables->tokens[index]); /* Convert the exit status string to an integer */
+
+            /* Check if the conversion failed (non-numeric input) */
+            if (close_status == -1)
+            {
+                input_variables->status = 2; /* Set exit status to 2 (indicating an error) */
+                _error(input_variables, ": Unacceptable input: "); /* Call the error handling function */
+                str_out(input_variables->tokens[index]); /* Print the problematic input */
+                str_out("\n"); /* Print a newline character */
+                free(input_variables->commands); /* Free allocated memory */
+                input_variables->commands = NULL; /* Set pointer to NULL */
+                return; /* Exit the function */
+            }
+
+            input_variables->status = close_status; /* Set the exit status to the provided status */
+        }
+        index++; // Move to the next token
+    }
+
+    /* Free allocated memory */
+    free(input_variables->buffer);
+    free(input_variables->tokens);
+    free(input_variables->commands);
+    free_environ(input_variables->env);
+
+    exit(input_variables->status); /* Exit the program with the specified exit status */
+}
