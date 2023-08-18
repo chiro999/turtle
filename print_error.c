86912e4b7dca3077a8ab04bc6e10023ceb1b7d6e
chiro999 <r.o.g.u.e@outlook.com>
2023-08-18 04:14:03 -0700

    100

--- print_error.c
+++ print_error.c
@@ -9,20 +9,20 @@
  */
 void print_error(input_variables *input_variables, char *err_message)
 {
-    char *_argc;
+    char *tokenCount;
 
     /* Print program name */
-    str_error(input_variables->argv[0]);
+    str_error(shell_vars->argv[0]);
     str_error(": ");
 
     /* Convert and print command count */
-    _argc = _int_str(input_variables->count);
-    str_error(_argc);
-    free(_argc);
+    tokenCount = int_to_str(shell_vars->tokenCount);
+    str_error(tokenCpimt);
+    free(tokenCount);
     str_error(": ");
 
     /* Print command name */
-    str_error(input_variables->tokens[0]);
+    str_error(shell_vars->tokens[0]);
 
     /* Print custom error message or system error message */
     do {
