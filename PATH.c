4de1ea47b8fec2e6687e0050c26717ed6e14eaab
chiro999 <r.o.g.u.e@outlook.com>
2023-08-19 19:19:53 -0700

    path

--- PATH.c
+++ PATH.c
@@ -86,3 +86,86 @@ char *str_path(char **env_arr)
 
     return (env_arr[i]);
 }
+
+int exec_curr_dir(shell_t *shell_vars)
+{
+    pid_t child_process;
+
+    if (access(shell_vars->tokens[0], F_OK) == 0)
+    {
+        if (access(shell_vars->tokens[0], X_OK) == 0)
+        {
+            child_process = fork();
+            if (child_process == -1)
+                print_error(shell_vars, NULL);
+            if (child_process == 0)
+            {
+                if (execve(shell_vars->tokens[0], shell_vars->tokens, shell_vars->env_vars) == -1)
+                    print_error(shell_vars, NULL);
+            }
+            else
+            {
+                wait(&shell_vars->close_status);
+                if (WIFEXITED(shell_vars->close_status))
+                    shell_vars->close_status = WEXITSTATUS(shell_vars->close_status);
+                else if (WIFSIGNALED(shell_vars->close_status) && WTERMSIG(shell_vars->close_status) == SIGINT)
+                    shell_vars->close_status = 130;
+                return 0;
+            }
+            shell_vars->close_status = 127;
+            return 1;
+        }
+        else
+        {
+            print_error(shell_vars, ": Permission denied\n");
+            shell_vars->close_status = 126;
+        }
+        return 0;
+    }
+    print_error(shell_vars, ": not found\n");
+    shell_vars->close_status = 127;
+    return 0;
+}
+
+void path_check(shell_vars *shell_vars)
+{
+    char *path, *replica = NULL, *check = NULL;
+    unsigned int i = 0, run_cmd = 0;
+    char **path_tokens;
+
+    if (check_dir(shell_vars->tokens[0]))
+        run_cmd = exec_cwd(shell_vars);
+    else
+    {
+        path = find_path(shell_vars->env_vars);
+        if (path != NULL)
+        {
+            replica = _strdup(path + 5);
+            path_tokens = custom_tokenizer(replica, ":");
+            for (i = 0; path_tokens && path_tokens[i]; i++, free(check))
+            {
+                check = _strcat(path_tokens[i], shell_vars->tokens[0]);
+                if (access(check, F_OK) == 0)
+                {
+                    run_cmd = _execute(check, shell_vars);
+                    free(check);
+                    break;
+                }
+            }
+            free(replica);
+            if (path_tokens == NULL)
+            {
+                shell_vars->close_status = 127;
+                _close(shell_vars);
+            }
+        }
+        if (path == NULL || path_tokens[i] == NULL)
+        {
+            print_error(shell_vars, ": not found\n");
+            shell_vars->close_status = 127;
+        }
+        free(path_tokens);
+    }
+    if (run_cmd == 1)
+        _close(shell_vars);
+}
