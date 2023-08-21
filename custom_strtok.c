d75a7f21d50f28ac0754edff99970381995a2f90
chiro999 <r.o.g.u.e@outlook.com>
2023-08-21 07:20:04 -0700

    120

--- custom_strtok.c
+++ custom_strtok.c
@@ -28,57 +28,40 @@ char *custom_strtok(char *str, const char *delim)
     static char *next_token;
     unsigned int i;
 
-    /* Initialize 'next_token' with 'str' if provided */
     if (str != NULL)
         next_token = str;
 
-    /* Store the current value of 'next_token' in 'token' */
     token = next_token;
 
-    /* If 'token' is NULL, there are no more tokens, so return NULL */
-    if (token == NULL)
-        return (NULL);
+    if (!token)
+        return NULL;
 
-    /* Find the start of the next token by skipping over delimiter characters */
-    for (i = 0; token[i] != '\0'; i++)
-    {
-        if (is_a_match(token[i], delim) == 0)
-            break;
-    }
+    i = 0;
+    while (token[i] != '\0' && is_a_match(token[i], delim) == 0)
+        i++;
 
-    /* If the end of the string is reached, there are no more tokens */
     if (token[i] == '\0')
     {
         next_token = NULL;
-        return (NULL);
+        return NULL;
     }
 
-    /* Update 'token' to point to the beginning of the next token */
     token += i;
 
-    /* Find the end of the token by searching for the next delimiter */
-    for (i = 0; token[i] != '\0'; i++)
-    {
-        if (is_a_match(token[i], delim) == 1)
-            break;
-    }
+    i = 0;
+    while (token[i] != '\0' && is_a_match(token[i], delim) == 1)
+        i++;
 
-    /* If the end of the string is reached, set 'next' to NULL */
     if (token[i] == '\0')
         next_token = NULL;
     else
     {
-        /* Replace the delimiter character with a null terminator */
         token[i] = '\0';
-
-        /* Update 'next_token' to point to the character after the delimiter */
         next_token = token + i + 1;
 
-        /* If the next character is the end of the string, set 'next_token' to NULL */
         if (*next_token == '\0')
             next_token = NULL;
     }
 
-    /* Return a pointer to the current token */
-    return (token);
+    return token;
 }
